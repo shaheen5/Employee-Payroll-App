a374bd4fbb005bfe7d8229ba8ce6d9c24945ed8c
shaheen5 <miyashaheen1995@gmail.com>
2021-05-26 23:04:40 +0530

    Added : Code To Reset Form When Reset Button Is Clicked

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -103,3 +103,28 @@ function createAndUpdateStorage(employeePayrollData){
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
+
+//reset form function to reset all elements in html form
+const resetForm=()=>{
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2021'); 
+}
+
+const unsetSelectedValues = (propertyValue)=>{
+    let allItems=document.querySelectorAll(propertyValue);
+    allItems.forEach(item=>{
+        item.checked = false;
+    });
+}
+
+const setValue=(id,value)=>{
+    const element = document.querySelector(id);
+    element.value = value;
+}
